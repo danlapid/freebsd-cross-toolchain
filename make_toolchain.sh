d42263de0e40850cf04c1336a694338bde06b0c2
Dan Lapid <dan.lapid@gmail.com>
2023-12-28 17:34:55 +0200

    Build many toolchains

--- make_toolchain.sh
+++ make_toolchain.sh
@@ -3,14 +3,31 @@
 sudo apt-get update
 sudo apt-get install -y build-essential wget git flex bison texinfo coreutils diffutils gcc gettext make perl sed binutils libgmp3-dev libmpc-dev libmpfr-dev libisl-dev
 
-export TARGET=i386-freebsd10
-export PREFIX=$PWD/cross-freebsd
-export PATH=$PREFIX/bin:$PATH
+: ${ARCH:=i386}
+: ${OS:=freebsd}
+: ${OSVERSION:=10}
+: ${GCC_VERSION:=13.2.0}
+: ${BINUTILS_VERSION:=2.41}
+: ${TARGET:=$ARCH-$OS$OSVERSION}
+: ${PREFIX:=$PWD/cross-freebsd}
+: ${PATH:=$PREFIX/bin:$PATH}
+if [ $ARCH = "x86_64" ]; then
+    : ${ARCH_ALIAS:=amd64}
+else
+    : ${ARCH_ALIAS:=$ARCH}
+fi
 mkdir -p $PREFIX{,/lib,/usr/lib,/usr/include}
 
-wget http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/i386/10.0-RELEASE/base.txz
+
 mkdir base/
-tar -xf base.txz -C base/
+if [ $OSVERSION -lt 9 ]; then
+  wget -r -nd -A "base.*" ftp://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/$ARCH_ALIAS/$OSVERSION.0-RELEASE/base/
+  cat base.?? | tar -xpzf - -C base/
+else
+  wget http://ftp-archive.freebsd.org/pub/FreeBSD-Archive/old-releases/$ARCH_ALIAS/$OSVERSION.0-RELEASE/base.txz
+  tar -xpf base.txz -C base/
+fi
+
 cp -r base/usr/include $PREFIX/usr/
 cp -r base/usr/lib $PREFIX/usr/
 cp -r base/lib $PREFIX/
@@ -18,24 +35,24 @@ pushd $PREFIX/usr/lib
 find . -xtype l | xargs ls -l | grep ' /lib/' | awk '{print "ln -sf ../.." $11 " " $9}' | /bin/sh
 popd
 
-wget https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz
-tar -xf binutils-2.41.tar.xz
-cd binutils-2.41
+wget https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz
+tar -xf binutils-$BINUTILS_VERSION.tar.xz
+cd binutils-$BINUTILS_VERSION
 mkdir build; cd build
 ../configure --enable-libssp --enable-gold --enable-ld --target=$TARGET --prefix=$PREFIX --with-sysroot=$PREFIX --disable-multilib
 make -j 3
 make install
 cd ../../
 
-wget https://gcc.gnu.org/pub/gcc/releases/gcc-13.2.0/gcc-13.2.0.tar.xz
-tar -xf gcc-13.2.0.tar.xz
-cd gcc-13.2.0
+wget https://gcc.gnu.org/pub/gcc/releases/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
+tar -xf gcc-$GCC_VERSION.tar.xz
+cd gcc-$GCC_VERSION
 ./contrib/download_prerequisites
 mkdir build; cd build
-../configure --without-headers --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-nls --enable-libssp --enable-gold --enable-ld --target=$TARGET --prefix=$PREFIX --disable-libgomp --host=x86_64-pc-linux-gnu --with-sysroot=$PREFIX --disable-multilib --disable-libsanitizer
+../configure --without-headers --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-nls --enable-libssp --enable-gold --enable-ld --target=$TARGET --prefix=$PREFIX --disable-libgomp --with-sysroot=$PREFIX --disable-multilib --disable-libsanitizer
 LD_LIBRARY_PATH=$PREFIX/lib make -j 2
 make install
 cd ../../
 
 tar -czf $TARGET.tar.gz -C $PREFIX .
-echo "FreeBSD 10 cross-compilation toolchain created successfully!"
+echo "FreeBSD cross-compilation toolchain created successfully!"
